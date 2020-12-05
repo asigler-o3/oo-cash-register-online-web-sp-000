6f991d04fee1e5d2a3903428374fa3d93a26eb12
Ashley Sigler <ashleysigler13@gmail.com>
2020-12-05 22:32:06 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -33,7 +33,7 @@ class CashRegister
   end
 
   def void_last_transaction
-    self.total = self.total - self.last_transaction
+    self.total = self.total - self.void_last_transaction
   end
 
 end
